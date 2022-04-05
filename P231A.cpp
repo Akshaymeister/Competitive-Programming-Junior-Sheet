711d7c0099442a51c5cf868e48546931ccc9d7a8
Akshay <akshaynigam90@gmail.com>
2022-04-05 12:37:29 +0530

    Problem 4 from sheet

---
+++ P231A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define long long int
+
+void solve()
+{
+    int n; cin >> n;
+    int ans=0;
+    for (int i = 0; i < n;i++)
+    {
+        int a,b,c; cin>>a>>b>>c;
+        if(a+b+c >=2)
+            ans+=1;
+    }
+    cout<<ans;
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t=1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
