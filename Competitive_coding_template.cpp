6a2b590a68b5df607eed5722819783f6786815d1
Akshay <akshaynigam90@gmail.com>
2022-04-18 19:30:19 +0530

    This is Version 0.1B of my competive coding template.

---
+++ Competitive_coding_template.cpp
@@ -0,0 +1,50 @@
+/*Author : Akshay 
+Sometimes you'll feel the progress has stopped that's the time you
+should trust your gut and keep going.
+Competitive coding ain't a sprint buddy its a marathon. */
+// Use C++17!
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+// loops macros
+#define rep(i,n) for (i = 0; i < n; ++i)
+#define REP(i,k,n) for (i = k; i <= n; ++i)
+#define REPR(i,k,n) for (i = k; i >= n; --i)
+// vectors macros
+// size(x), rbegin(x), rend(x) need C++17
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend() 
+#define sor(x) sort(all(x)) 
+#define rsz resize
+#define ins insert 
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+/*-----------------------------------------------------------------
+CODE GOES BELOW!!!*/
+void solve()//<-- start writing in this function!
+{
+ 
+}
+  signed main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  int t=1;
+  // cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+}
+/* stuff you should look for
+  * int overflow, array bounds
+  * special cases (n=1?)
+  * do smth instead of nothing and stay organized
+  * WRITE STUFF DOWN
+  * DON'T GET STUCK ON ONE APPROACH
+*/
